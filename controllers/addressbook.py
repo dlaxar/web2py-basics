3587fd3ba254fb0baffd9da91cb2d7c35cb5624f
Daniel Laxar <i.dlaxar@gmail.com>
2013-10-30 09:21:13 +0100

    Styles and SQLFORM (add)

--- controllers/addressbook.py
+++ controllers/addressbook.py
@@ -2,5 +2,43 @@
 
 def index():
 
-	#             where       select (nix = *)
-	return dict(data=db(db.contacts.id > 0).select())
+	data = None
+	# wenn parameter -> zeig nur den kontakt an
+	if len(request.args) > 0:
+		data = db(db.contacts.id == request.args[0]).select()
+	
+	# wenn kein parameter -> Liste
+	else:
+		data = db(db.contacts.id > 0).select()
+
+	return dict(data=data)
+
+
+# def display_form():
+#    form = SQLFORM(db.person)
+#    if form.process().accepted:
+#        response.flash = 'form accepted'
+#    elif form.errors:
+#        response.flash = 'form has errors'
+#    else:
+#        response.flash = 'please fill out the form'
+#    return dict(form=form)
+
+def add():
+	form = SQLFORM(db.contacts)
+
+	return dict(form=form)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
